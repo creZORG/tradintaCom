d3d99f3a104b65a889153c82ec9989e315b7ca38
luna industries <lunaindustrieske@gmail.com>
2025-11-26 06:34:27 +0000

    I just deleted the next folder, maybe you need to do somthing
    here is

---
+++ next.config.ts
@@ -0,0 +1,101 @@
+import 'dotenv/config';
+import type {NextConfig} from 'next';
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com js.paystack.co;
+  child-src 'self';
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+  img-src 'self' data: placehold.co images.unsplash.com picsum.photos i.postimg.cc res.cloudinary.com;
+  media-src 'self';
+  connect-src *;
+  font-src 'self' fonts.gstatic.com;
+`;
+
+const nextConfig: NextConfig = {
+  srcDir: 'src',
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+           {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          }
+        ],
+      },
+    ];
+  },
+  /* config options here */
+  env: {
+    NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY: process.env.PAYSTACK_PUBLIC_KEY,
+  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
